c9f3e56ae55ddb1bf0aee3033eb63d8156810bfe
Gabriel Laurindo <gabrielvitorlaurindo@gmail.com>
2021-03-07 19:21:17 -0300

    Finalizando os testes por hoje

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
-    res.send({"API": "Teste Node.js", "Descrição":"Feita para estudo."})
+    res.send({
+        API: "Teste Node.js", 
+        Descrição:"Feita para estudo."
+    })
 });
 
 module.exports = router;
