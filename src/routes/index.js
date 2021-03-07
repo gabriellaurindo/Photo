e2afd234da81986746b533a427b0f6f48b9d93eb
Gabriel Laurindo <gabrielvitorlaurindo@gmail.com>
2021-03-07 19:01:30 -0300

    Primeira tentativa de uma API JSON

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
-    res.send('Hello word')
+    res.send({"API": "Teste Node.js", "Descrição":"Feita para estudo."})
 });
 
 module.exports = router;
